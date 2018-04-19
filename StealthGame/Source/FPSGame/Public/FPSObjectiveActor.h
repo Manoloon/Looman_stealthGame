adb27ad0501b3a66f9fbead825e907178eb62780
mordor\manoloon <pablitensehen@gmail.com>
2018-04-18 22:37:09 -0300

    blackhole prototype

--- StealthGame/Source/FPSGame/Public/FPSObjectiveActor.h
+++ StealthGame/Source/FPSGame/Public/FPSObjectiveActor.h
@@ -24,6 +24,7 @@ protected:
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 		USphereComponent* SphereComp;
 
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
